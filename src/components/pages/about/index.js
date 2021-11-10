64db4048f6ae7b46ee0d5d768c4d2b0efdb3eab9
Jon Brogdon <jon.brogdon@gmail.com>
2021-11-10 13:40:24 -0600

    fix wrong case

--- src/components/pages/about/index.js
+++ src/components/pages/about/index.js
@@ -6,7 +6,7 @@ import './about.scss';
 export function About(props) {
     return (
         <div className="about-page">
-            <Paragraph>I'm candid. I'm friendly. I'm helpful. I've got a great family. I'm self-taught. I'm articulate. I actively communicate. I will answer almost any question honestly. I started this page because you need a portfolio to be marketable. I'm versed in Node.JS and Tcl. If you don't know Tcl, then you probably don't know it's colloquially pronounced "tickle".</Paragraph>
+            <Paragraph>I'm candid. I'm friendly. I'm helpful. I've got a great family. I'm self-taught. I'm articulate. I actively communicate. I will answer almost any question honestly. I started this page because you need a portfolio to be marketable. I'm versed in Node.js and Tcl. If you don't know Tcl, then you probably don't know it's colloquially pronounced "tickle".</Paragraph>
         </div>
     );
 }
