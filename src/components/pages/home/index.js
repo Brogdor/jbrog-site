e9c94044a5808d7df3212fa789cbe197c542234e
Jon Brogdon <jon.brogdon@gmail.com>
2021-11-10 12:56:18 -0600

    add external links

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -6,12 +6,12 @@ import './home.scss';
 
 export function Home(props) {
     return (
+
         <div className="home-page">
-            <div>
-                <Text>Jon Brogdon, Software Engineer</Text>
-            </div>
-            <div>
-                <Link to="/about">About</Link>
+            <Text>Jon Brogdon, Software Engineer</Text>
+            <div className="link-list">
+                <Link to="https://github.com/brogdor">Github</Link>
+                <Link to="https://linkedin.com/in/brogdor">LinkedIn</Link>
             </div>
         </div>
     );
